9976886fabd0d45ac95f29fab02d546266f8a172
Ben DiFrancesco <ben@scopelift.co>
2025-03-21 10:57:41 -0400

    Update the way fixed address aliases are calculated
    This change prevents a user from calculating the "inverse offset" of their address,
    and therefore prevents a holder of the Fixed LST from sending tokens to this inverse
    address. If a user were to do so, these tokens would exit the Fixed LST and return to
    the Rebasing LST, but without the shares being properly removed from the total supply
    of the Fixed LST. As a result, the Fixed LST total supply could reflect an unrealistic
    value.

--- src/FixedLstAddressAlias.sol
+++ src/FixedLstAddressAlias.sol
@@ -5,16 +5,20 @@ pragma solidity ^0.8.23;
 /// @author [ScopeLift](https://scopelift.co)
 /// @notice Library code for calculating the alias of an address.
 library FixedLstAddressAlias {
-  /// @notice The constant value that is added to an existing address to calculate its offset.
-  uint160 public constant ALIAS_OFFSET = 0x010101;
+  /// @notice The constant value that is added to an existing address to calculate its offset before it is hashed.
+  uint160 public constant ALIAS_OFFSET_SALT = 0x1000010100000110011011010010;
 
-  /// @notice Calculates the alias of a given address by adding a fixed constant to it.
+  /// @notice Calculates the alias of a given address by adding a fixed constant and hashing it.
   /// @param _account The address for which the alias will be calculated.
   /// @return _alias The calculated alias of the address provided.
+  /// @dev Adding a fixed constant before hashing acts as a kind of "salt", ensuring the address produced is not simply
+  /// the keccak256 of the address in question, as this might conflict in some unexpected way with some other
+  /// application. At the same time, adding the "salt" to the address, rather than concatenating it, avoids adding
+  /// extra bytes to the keccak operation, which would incur additional gas costs.
   function fixedAlias(address _account) internal pure returns (address _alias) {
-    /// Overflow is desireable as addresses that are close to 0xFF...FF will overflow to a lower alias address.
+    /// Overflow is desirable as addresses that are close to 0xFF...FF will overflow to a lower alias address.
     unchecked {
-      _alias = address(uint160(_account) + ALIAS_OFFSET);
+      _alias = address(uint160(uint256(keccak256(abi.encode(uint160(_account) + ALIAS_OFFSET_SALT)))));
     }
   }
 }
