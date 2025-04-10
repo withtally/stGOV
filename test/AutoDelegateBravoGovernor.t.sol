8349191477c5f8d8e19e8ba82215755aabc8fd18
John Feras <jferas@ferasinfotech.com>
2025-04-10 15:21:17 -0400

    Replaced FakeAutoDelegateBravoGovernor with an actual instance to increase test coverage

--- test/AutoDelegateBravoGovernor.t.sol
+++ test/AutoDelegateBravoGovernor.t.sol
@@ -4,14 +4,28 @@ pragma solidity ^0.8.23;
 import {Test} from "forge-std/Test.sol";
 import {GovernorBravoDelegateMock} from "./mocks/GovernorBravoDelegateMock.sol";
 import {IGovernorBravoDelegate} from "../src/interfaces/IGovernorBravoDelegate.sol";
-import {FakeAutoDelegateBravoGovernor} from "./fakes/FakeAutoDelegateBravoGovernor.sol";
+import {AutoDelegateBravoGovernor} from "../src/auto-delegates/extensions/AutoDelegateBravoGovernor.sol";
+
+contract InstanceOfAutoDelegateBravoGovernor is AutoDelegateBravoGovernor {
+  function exposed_castVote(address _governor, uint256 _proposalId) external {
+    _castVote(_governor, _proposalId);
+  }
+
+  function exposed_getProposalDetails(address _governor, uint256 _proposalId)
+    external
+    view
+    returns (uint256 _proposalDeadline, uint256 _forVotes, uint256 _againstVotes, uint256 _quorumVotes)
+  {
+    return _getProposalDetails(_governor, _proposalId);
+  }
+}
 
 contract AutoDelegateBravoGovernorTest is Test {
-  FakeAutoDelegateBravoGovernor public autoDelegate;
+  InstanceOfAutoDelegateBravoGovernor public autoDelegate;
   GovernorBravoDelegateMock public governor;
 
   function setUp() public {
-    autoDelegate = new FakeAutoDelegateBravoGovernor();
+    autoDelegate = new InstanceOfAutoDelegateBravoGovernor();
     governor = new GovernorBravoDelegateMock();
   }
 }
