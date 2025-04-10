495aee42fe647a0029f042d4c6bc61d8471eef21
John Feras <jferas@ferasinfotech.com>
2025-04-10 16:16:21 -0400

    Removed unneeded DeployUniLst.s.sol

--- src/script/DeployUniLst.s.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-// slither-disable-start reentrancy-benign
-pragma solidity ^0.8.23;
-
-import {Script} from "forge-std/Script.sol";
-
-contract Deploy is Script {
-  function run() public {}
-}
