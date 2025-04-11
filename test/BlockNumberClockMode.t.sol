04935f16ed9d9e5ec526a7c7dad22735c1c9b82b
John Feras <jferas@ferasinfotech.com>
2025-04-11 09:53:43 -0400

    Added testing of the clock consistency checks

--- test/BlockNumberClockMode.t.sol
+++ test/BlockNumberClockMode.t.sol
@@ -5,11 +5,27 @@ import {Test} from "forge-std/Test.sol";
 import {BlockNumberClockMode} from "../src/auto-delegates/extensions/BlockNumberClockMode.sol";
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
+contract MockBlockNumberClockMode is BlockNumberClockMode {
+  uint48 private mockClockValue;
+
+  /// @notice Allows overriding the clock value for testing.
+  function setMockClock(uint48 _mockClockValue) public {
+    mockClockValue = _mockClockValue;
+  }
+
+  /// @notice Overrides the clock function to return the mocked value.
+  function clock() public view override returns (uint48) {
+    return mockClockValue;
+  }
+}
+
 contract BlockNumberClockModeTest is Test {
   BlockNumberClockMode public blockNumberClockMode;
+  MockBlockNumberClockMode public mockBlockNumberClockMode;
 
   function setUp() public {
     blockNumberClockMode = new BlockNumberClockMode();
+    mockBlockNumberClockMode = new MockBlockNumberClockMode();
   }
 }
 
@@ -24,5 +40,16 @@ contract Clock is BlockNumberClockModeTest {
 contract CLOCK_MODE is BlockNumberClockModeTest {
   function test_ReturnsCorrectClockMode() public view {
     assertEq(blockNumberClockMode.CLOCK_MODE(), "mode=blocknumber&from=default");
+  }
+}
+
+contract CLOCK_MODE_Revert is BlockNumberClockModeTest {
+  function test_RevertIf_ClockInconsistent() public {
+    // Set a mock clock value inconsistent with the current block number
+    mockBlockNumberClockMode.setMockClock(SafeCast.toUint48(block.number + 1));
+
+    // Expect the ERC6372InconsistentClock error to be reverted
+    vm.expectRevert(BlockNumberClockMode.ERC6372InconsistentClock.selector);
+    mockBlockNumberClockMode.CLOCK_MODE();
   }
 }
