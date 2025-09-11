67662f1ed4f51d131b8f93be61cbdeaf57eb1f22
keating <keating.dev@protonmail.com>
2025-09-11 13:33:19 -0400

    Small cleanup

--- src/WrappedGovLst.sol
+++ src/WrappedGovLst.sol
@@ -88,104 +88,120 @@ contract WrappedGovLst is ERC20Permit, Ownable {
     }
 
     LST.transferFrom(msg.sender, address(this), _lstAmountToWrap);
-    // TODO: Add control flow for the off by 1 error
-    // I think this should error in the tests
     FIXED_LST.convertToFixed(_lstAmountToWrap);
-    // The fixed tokens are already scaled appropriately, so we just mint the same amount of wrapped tokens
     _wrappedAmount = previewWrapRebasing(_lstAmountToWrap);
     _mint(msg.sender, _wrappedAmount);
 
     emit WrappedRebasing(msg.sender, _lstAmountToWrap, _wrappedAmount);
   }
 
-  // Everything converts to the rebasing
-  // can convert out to different things
-  // Must approve to FIXED_LST
-  // There could be some rounding weirdness
+  /// @notice Deposit underlying stake tokens and receive wrapped tokens in exchange.
+  /// @param _stakeTokensToWrap The quantity of underlying stake tokens to wrap.
+  /// @return _wrappedAmount The quantity of wrapped tokens issued to the caller.
+  /// @dev The caller must approve at least the amount of tokens to wrap on the stake token contract before calling. 
+  /// Amount to wrap may not be zero.
   function wrapUnderlying(uint256 _stakeTokensToWrap) public virtual returns (uint256) {
     if (_stakeTokensToWrap == 0) {
       revert WrappedGovLst__InvalidAmount();
     }
 
-    // User must have approved the wrapper to spend their stake tokens
     IERC20 stakeToken = IERC20(address(LST.STAKE_TOKEN()));
     stakeToken.transferFrom(msg.sender, address(this), _stakeTokensToWrap);
-
-    // Approve FIXED_LST to transfer the stake tokens from wrapper
     stakeToken.approve(address(FIXED_LST), _stakeTokensToWrap);
 
-    // Calculate expected wrapped amount using preview
     uint256 _wrappedAmount = previewWrapUnderlying(_stakeTokensToWrap);
-
-    // Stake through FIXED_LST which will transfer tokens to LST and stake them
     FIXED_LST.stake(_stakeTokensToWrap);
-
-    // Mint wrapped tokens to the user
     _mint(msg.sender, _wrappedAmount);
 
-    // Emit event for consistency with wrap function
     emit WrappedUnderlying(msg.sender, _stakeTokensToWrap, _wrappedAmount);
 
     return _wrappedAmount;
   }
 
+  /// @notice Deposit fixed LST tokens and receive wrapped tokens in exchange.
+  /// @param _fixedTokensToWrap The quantity of FIXED_LST tokens to wrap.
+  /// @return _wrappedAmount The quantity of wrapped tokens issued to the caller.
+  /// @dev The caller must approve at least the amount of tokens to wrap on the FIXED_LST contract before calling. 
+  /// Amount to wrap may not be zero.
   function wrapFixed(uint256 _fixedTokensToWrap) external virtual returns (uint256) {
     if (_fixedTokensToWrap == 0) {
       revert WrappedGovLst__InvalidAmount();
     }
 
-    // Transfer fixed tokens from user to wrapper
     FIXED_LST.transferFrom(msg.sender, address(this), _fixedTokensToWrap);
 
-    // Calculate wrapped amount using preview (should be 1:1)
     uint256 _wrappedAmount = previewWrapFixed(_fixedTokensToWrap);
-
-    // Mint wrapped tokens to user
     _mint(msg.sender, _wrappedAmount);
 
-    // Emit event for consistency
     emit WrappedFixed(msg.sender, _fixedTokensToWrap, _wrappedAmount);
 
     return _wrappedAmount;
   }
 
-  /// Return the number of vault shares or less
-  function previewWrapRebasing(uint256 _stakeTokensToWrap) public view virtual returns (uint256) {
+  /// @notice Preview the amount of wrapped tokens that would be minted when wrapping rebasing LST tokens.
+  /// @param _rebasingTokensToWrap The amount of rebasing LST tokens to wrap.
+  /// @return The amount of wrapped tokens that would be minted.
+  /// @dev Returns the exact amount that would be minted by wrapRebasing.
+  function previewWrapRebasing(uint256 _rebasingTokensToWrap) public view virtual returns (uint256) {
     // Calculate the shares that will be transferred when converting to fixed
-    // This needs to match the rounding behavior of _calcSharesForStakeUp in GovLst
-    return _calcSharesForStakeUp(_stakeTokensToWrap) / SHARE_SCALE_FACTOR;
+    // This match the rounding behavior of _calcSharesForStakeUp in GovLst
+    return _calcSharesForStakeUp(_rebasingTokensToWrap) / SHARE_SCALE_FACTOR;
   }
 
-  // TODO: Does this cause rounding issues
+  /// @notice Preview the amount of wrapped tokens that would be minted when wrapping underlying stake tokens.
+  /// @param _stakeTokensToWrap The amount of underlying stake tokens to wrap.
+  /// @return The amount of wrapped tokens that would be minted.
+  /// @dev Simulates the staking process to determine the resulting wrapped token amount.
   function previewWrapUnderlying(uint256 _stakeTokensToWrap) public view virtual returns (uint256) {
-    // Calculate shares that will be created from staking (same as _calcSharesForStake)
-    if (LST.totalSupply() == 0) {
-      // First staker gets SHARE_SCALE_FACTOR * amount shares
-      return (SHARE_SCALE_FACTOR * _stakeTokensToWrap) / SHARE_SCALE_FACTOR;
-    }
-
-    // Fixed tokens are shares divided by SHARE_SCALE_FACTOR (same as _scaleDown in FixedGovLst)
-    return ((_stakeTokensToWrap * LST.totalShares()) / LST.totalSupply()) / SHARE_SCALE_FACTOR;
+    return _calcSharesForStake(_stakeTokensToWrap) / SHARE_SCALE_FACTOR;
   }
 
+  /// @notice Preview the amount of wrapped tokens that would be minted when wrapping fixed liquid staking tokens.
+  /// @param _fixedTokensToWrap The amount of fixed liquid staking tokens to wrap.
+  /// @return The amount of wrapped tokens that would be minted.
+  /// @dev Wrapped tokens maintain 1:1 backing with fixed liquid staking tokens.
   function previewWrapFixed(uint256 _fixedTokensToWrap) public view virtual returns (uint256) {
     return _fixedTokensToWrap;
   }
 
+  /// @notice Preview the amount of rebasing liquid stake tokens that would be received when unwrapping.
+  /// @param _wrappedAmount The amount of wrapped tokens to unwrap.
+  /// @return The amount of rebasing liquid stake tokens that would be received.
+  /// @dev Converts wrapped tokens to shares, then to rebasing tokens. Rounds down to favor the protocol.
   function previewUnwrapToRebase(uint256 _wrappedAmount) public view virtual returns (uint256) {
-    // Wrapped tokens are 1:1 with FIXED_LST tokens
-    // Convert to shares by scaling up
     uint256 _shares = _wrappedAmount * SHARE_SCALE_FACTOR;
-    // Convert shares to rebasing tokens, rounding down to favor the protocol
     return _calcStakeForShares(_shares);
   }
 
+  /// @notice Preview the amount of fixed liquid staking tokens that would be received when unwrapping.
+  /// @param _wrappedAmount The amount of wrapped tokens to unwrap.
+  /// @return The minimum amount of fixed liquid staking tokens that would be received.
+  /// @dev Returns a conservative estimate (1 wei less) to account for potential rounding in FIXED_LST transfer.
+  /// The actual amount received may be up to 1 wei more due to FIXED_LST's internal rounding behavior.
   function previewUnwrapToFixed(uint256 _wrappedAmount) public view virtual returns (uint256) {
     // At worst 1 wei less than what has been requested will be returned.
     // The preview will return the minimum amount of assets returned.
     return _wrappedAmount - 1;
   }
 
+  /// @notice Calculate the number of shares that would be created from staking a given amount.
+  /// @param _amount The amount of stake tokens to convert to shares.
+  /// @return The number of shares that would be created.
+  /// @dev Mirrors the share calculation logic from GovLst. Rounds down.
+  function _calcSharesForStake(uint256 _amount) internal view virtual returns (uint256) {
+    if (LST.totalSupply() == 0) {
+      // First staker gets SHARE_SCALE_FACTOR * amount shares
+      return SHARE_SCALE_FACTOR * _amount;
+    }
+
+    // Calculate shares proportionally to existing supply/shares ratio
+    return (_amount * LST.totalShares()) / LST.totalSupply();
+  }
+
+  /// @notice Calculate the amount of stake tokens that correspond to a given number of shares.
+  /// @param _shares The number of shares to convert to stake tokens.
+  /// @return The amount of stake tokens that the shares represent.
+  /// @dev Converts shares back to stake token amounts. Rounds down to favor the protocol.
   function _calcStakeForShares(uint256 _shares) internal view virtual returns (uint256) {
     if (LST.totalShares() == 0) {
       return _shares / SHARE_SCALE_FACTOR;
@@ -195,13 +211,15 @@ contract WrappedGovLst is ERC20Permit, Ownable {
     return (_shares * LST.totalSupply()) / LST.totalShares();
   }
 
+  /// @notice Calculate the number of shares for a stake amount, rounding up.
+  /// @param _amount The amount of stake tokens to convert to shares.
+  /// @return The number of shares that would be created, rounded up.
+  /// @dev Similar to _calcSharesForStake but rounds up if there's any remainder.
+  /// This ensures no value is lost when converting rebasing tokens to fixed tokens.
   function _calcSharesForStakeUp(uint256 _amount) internal view virtual returns (uint256) {
-    if (LST.totalSupply() == 0) {
-      return SHARE_SCALE_FACTOR * _amount;
-    }
+    uint256 _result = _calcSharesForStake(_amount);
 
-    uint256 _result = (_amount * LST.totalShares()) / LST.totalSupply();
-
+    // Add 1 if there's any remainder from the division
     if (mulmod(_amount, LST.totalShares(), LST.totalSupply()) > 0) {
       _result += 1;
     }
